759c7f1ebb656f6dc2e3c07e134928cef8a1e115
ahyom <atrpaula@gmail.com>
2023-08-19 12:51:16 -0300

    update variables to public

--- src/main/kotlin/com/soave/rinhabackend/domain/request/PessoaRequest.kt
+++ src/main/kotlin/com/soave/rinhabackend/domain/request/PessoaRequest.kt
@@ -7,20 +7,20 @@ import java.util.UUID
 
 class PessoaRequest(
 
-    private var id: UUID?,
+    var id: UUID?,
 
     @field:NotBlank(message = "Apelido é obrigatório")
     @field:Size(max = 32, message = "Campo apelido deve ter no máximo 32 caracteres")
-    private var apelido: String,
+    var apelido: String,
 
     @field:NotBlank(message = "Nome é obrigatório")
     @field:Size(max = 100, message = "Campo nome deve ter no máximo 100 caracteres")
-    private var nome: String,
+    var nome: String,
 
     @field:NotBlank(message = "Data de nascimento é obrigatória")
-    private var nascimento: String,
+    var nascimento: String,
 
-    private var stack: List<String>?,
+    var stack: List<String>?,
 ) {
     fun validate(): List<String> {
         val validator = Validation.buildDefaultValidatorFactory().validator
