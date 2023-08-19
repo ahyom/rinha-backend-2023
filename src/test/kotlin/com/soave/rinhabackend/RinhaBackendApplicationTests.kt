a139307c1d928f9769dec22ab5fb202f9e660740
ahyom <atrpaula@gmail.com>
2023-08-19 11:02:29 -0300

    scafold - generating project and files

---
+++ src/test/kotlin/com/soave/rinhabackend/RinhaBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.soave.rinhabackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RinhaBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
