3c47ad0dac70857f1b6c67d206e6525a10f5abd5
AhYom <atrpaula@gmail.com>
2024-10-02 18:13:56 -0300

    scaffolding project

---
+++ src/main/java/com/soave/backend_performance_challenge/BackendPerformanceChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.soave.backend_performance_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPerformanceChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPerformanceChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soave/backend_performance_challenge/BackendPerformanceChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soave.backend_performance_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPerformanceChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
