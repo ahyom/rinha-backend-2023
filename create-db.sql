62f8137609fba4983a3146c274fc54d1c96b7c35
AhYom <atrpaula@gmail.com>
2024-10-08 19:05:09 -0300

    provisioning infra with nginx

---
+++ create-db.sql
@@ -0,0 +1,1 @@
+create database "backend-battle-23"
---
+++ src/main/java/com/soave/backend_performance_challenge/config/DataSourceConfig.java
@@ -0,0 +1,64 @@
+package com.soave.backend_performance_challenge.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(DataSourceConfig.class);
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.hikari.maximum-pool-size}")
+    private int maxPoolSize;
+
+    @Value("${spring.datasource.hikari.minimum-idle}")
+    private int minIdle;
+
+    @Value("${spring.datasource.hikari.idle-timeout}")
+    private long idleTimeout;
+
+    @Value("${spring.datasource.hikari.connection-timeout}")
+    private long connectionTimeout;
+
+    @Value("${spring.datasource.hikari.max-lifetime}")
+    private long maxLifetime;
+
+    @Bean
+    public DataSource dataSource() {
+
+        log.info("Creating connection with database [{}]", dbUrl);
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setMaximumPoolSize(maxPoolSize);
+        dataSource.setMinimumIdle(minIdle);
+        dataSource.setIdleTimeout(idleTimeout);
+        dataSource.setConnectionTimeout(connectionTimeout);
+        dataSource.setMaxLifetime(maxLifetime);
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
